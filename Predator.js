6473659cca36a237ce71781a17b21fb350b85199
Jamie Duke <jamsduke@gmail.com>
2016-07-27 10:49:43 -0500

    finished listed instruction

--- Predator.js
+++ Predator.js
@@ -1,4 +1,26 @@
 var Predator = (function () {
   var carnivores = []
   var herbivores = []
+
+  return {
+    getCarnivores: function (cb) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'carnivores.json')
+      xhr.addEventListener('load', function (evt) {
+        carnivores = JSON.parse(evt.target.responseText)
+        cb(carnivores)
+      })
+      xhr.send()
+      // store it in the private varriable
+    },
+    getHerbivores: function (cb) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'herbivores.json')
+      xhr.addEventListener('load', function (evt) {
+        herbivores = JSON.parse(evt.target.responseText)
+        cb(herbivores)
+      })
+      xhr.send()
+    }
+  }
 }())
